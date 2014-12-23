b68c542b26dbea184b4f53c510beefb55fe6bac2
Snakevil Zen <zsnakevil@gmail.com>
2014-12-23 10:25:01 +0800

    增加基础网页／终端应用程序组件。

---
+++ lib/Console.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 定义基础终端应用程序组件。
+ *
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2014 SZen.in
+ * @license   LGPL-3.0+
+ */
+
+namespace snakevil\zen;
+
+use Zen\Console as ZenApp;
+
+/**
+ * 基础终端应用程序组件。
+ *
+ * @package snakevil\zen
+ * @since   0.1.0
+ */
+class Console extends ZenApp\Application
+{
+}
---
+++ lib/WebApp.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * 定义基础网页应用程序组件。
+ *
+ * @author    Snakevil Zen <zsnakevil@gmail.com>
+ * @copyright © 2014 SZen.in
+ * @license   LGPL-3.0+
+ */
+
+namespace snakevil\zen;
+
+use Zen\Web\Application as ZenApp;
+
+/**
+ * 基础网页应用程序组件。
+ *
+ * @package snakevil\zen
+ * @since   0.1.0
+ */
+class WebApp extends ZenApp\Application
+{
+}
